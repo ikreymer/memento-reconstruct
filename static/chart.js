5229e74486d76e1710a943a31411f65e10f33823
Ilya Kreymer <ikreymer@gmail.com>
2015-02-07 22:03:22 -0800

    new c3-based ui work, refactoring ui

--- static/chart.js
+++ static/chart.js
@@ -9,6 +9,55 @@ function agg_by_host(data) {
 }
 
 function init_host_chart(memento_list) {
+  
+  var agg = agg_by_host(memento_list);
+  var cols = [];
+  for (var i = 0; i < agg.length; i++) {
+    cols.push([agg[i].key + " (" + agg[i].values + ")",
+               agg[i].values]);
+  }
+  
+  var chart = c3.generate({
+    bindto: '#hostchart',
+    
+    tooltip: {
+//      format: {
+//        value: function (value, ratio, id) { return value; },
+//        name: function (name, ratio, id, index) { return name; },
+//      },
+    },
+
+    data: {
+        columns: cols,
+//      columns: [
+//        ['hostA (30)', 30],
+//        ['hostB (120)', 120],
+//        ['web.archive.org (150)', 150],
+//        ['web.archive.org (10)', 10],
+//        ['archive-it (100)', 56],
+//        ['ukblah (55)', 55],
+//        ['abc (123)', 123],
+//      ],
+      
+      type : 'pie',
+    },
+
+    pie: {
+      label: {
+        show: false,
+      },
+      expand: false
+    },
+    legend: {
+      position: 'right',
+    },
+  });
+  
+  chart.resize({width: 400});
+  d3.select("#hostchart svg").transition().duration(350);
+}
+
+function init_host_chart2(memento_list) {
     nv.addGraph(function() {
       var chart = nv.models.pieChart()
       //var chart = nv.models.discreteBarChart()
@@ -76,60 +125,68 @@ function init_scatter(memento_list, curr_memento) {
     });
 }
 
-
-function load_embed_viz(url, timestamp)
+function init_banner()
 {
-    var full_url = "/api/" + timestamp + "/" + url;
-
-    d3.json(full_url, function(error, json) {
-        if (error) {
-            console.log(error);
-            return;
-        }
-
-        var mementos = [];
-        var curr_only = [];
-
-        var target_sec = parseInt(json["_target_sec"]);
-
-        for (var key in json) {
-            if (key == "_target_sec") {
-                continue;
-            }
-
-            var list = key.split(" ");
-            var sec = parseInt(json[key]) - target_sec;
+  console.log("inited");
+}
 
 
-            var point = {"host": list[0],
-                         "ts": list[1],
-                         "url": list[2],
+function update_banner(info)
+{
+  var url = info.url;
+  var timestamp = info.timestamp;
+  
+  var full_url = "/api/" + timestamp + "/" + url;
 
-                         "x": sec,
-                         "y": Math.random() * 4.0 - 2.0,
-                         "shape": "diamond",
-                         "size": 5.0};
+  d3.json(full_url, function(error, json) {
+      if (error) {
+          console.log(error);
+          return;
+      }
 
-            if (sec == 0 && point.url == url) {
-                point["shape"] = "circle";
-                point["size"] = 10;
-                point["y"] = 0;
-                curr_only = [point];
-            } else {
-                mementos.push(point);
-            }
-        }
+      var mementos = [];
+      var curr_only = [];
 
-        if (curr_only.length > 0 || mementos.length > 0) {
-            init_scatter(mementos, curr_only);
+      var target_sec = parseInt(json["_target_sec"]);
 
-            //combine for host chart
-            if (curr_only.length > 0) {
-                mementos.push(curr_only[0]);
-            }
-            init_host_chart(mementos)
-        }
-    });
+      for (var key in json) {
+          if (key == "_target_sec") {
+              continue;
+          }
+
+          var list = key.split(" ");
+          var sec = parseInt(json[key]) - target_sec;
+
+
+          var point = {"host": list[0],
+                       "ts": list[1],
+                       "url": list[2],
+
+                       "x": sec,
+                       "y": Math.random() * 4.0 - 2.0,
+                       "shape": "diamond",
+                       "size": 5.0};
+
+          if (sec == 0 && point.url == url) {
+              point["shape"] = "circle";
+              point["size"] = 10;
+              point["y"] = 0;
+              curr_only = [point];
+          } else {
+              mementos.push(point);
+          }
+      }
+
+      if (curr_only.length > 0 || mementos.length > 0) {
+          //init_scatter(mementos, curr_only);
+
+          //combine for host chart
+          if (curr_only.length > 0) {
+              mementos.push(curr_only[0]);
+          }
+          init_host_chart(mementos);
+      }
+  });
 }
 
 
